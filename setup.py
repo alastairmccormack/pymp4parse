6705b4eb603f69681357a5f71f02e81705ea5e17
GordonSo <gordonkwso@gmail.com>
2017-04-24 18:43:35 +0100

    Add six as dependency to fix import issue

--- setup.py
+++ setup.py
@@ -15,8 +15,8 @@ setup(
     author='Alastair Mccormack',
     author_email='alastair at alu.media',
     description='MP4 / ISO base media file format (ISO/IEC 14496-12 - MPEG-4 Part 12) file parser',
-    requires=['bitstring'],
-    install_requires=['bitstring'],
+    requires=['bitstring', 'six'],
+    install_requires=['bitstring', 'six'],
     long_description=long_description,
     data_files=[('', ['README.md'])]
 )
